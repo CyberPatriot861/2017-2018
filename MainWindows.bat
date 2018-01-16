3f7fe8954ca6db23e470e94db1c312880ab26055
TheSmallGamer <jamiesmall0718@gmail.com>
2018-01-16 13:48:24 -0600

    Update MainWindows.bat

--- MainWindows.bat
+++ MainWindows.bat
@@ -35,6 +35,36 @@ if "%func%"=="exit" exit
 call:%func%
 goto:manual
 
+:uptFirefox
+set "FirefoxVersion=37"
+rem Get path of installed Firefox directly from Windows registry.
+for /F "skip=2 tokens=1,2*" %%A in ('%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows\CurrentVersion\App Paths\firefox.exe" /v Path 2^>nul') do (
+    if "%%A"=="Path" (
+        set "FirefoxFolder=%%C"
+        goto CheckFirefox
+    )
+)
+
+:InstallFirefox
+echo Installing Firefox ...
+
+:UpdateFireFox
+\\***\***\Firefox-Setup-%FirefoxVersion%.exe -ms
+goto :EOF
+
+:CheckFirefox
+if not exist "%FirefoxFolder%\firefox.exe" goto InstallFirefox
+
+rem Check if version of Mozilla Firefox starts with defined number.
+rem The space at beginning makes sure to find the major version number.
+"%FirefoxFolder%\firefox.exe" -v | %SystemRoot%\System32\more | %SystemRoot%\System32\find.exe " %FirefoxVersion%" >nul
+if errorlevel 1 (
+    echo Updating Firefox to version %FirefoxVersion% ...
+    goto UpdateFireFox
+)
+
+echo Firefox with version %FirefoxVersion% is already installed.
+
 :checkFiles
 start C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "%path%resources\Check_Files.ps1" /wait
 goto:EOF
